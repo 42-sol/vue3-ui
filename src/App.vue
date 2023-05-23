c475db289f39ef574aba375be4f93265d14d4fe9
dmrompav <dm.rom.pav@gmail.com>
2023-05-23 23:19:44 +0300

    alpha.2 force new lib

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script lang='ts'>
+import { defineComponent } from 'vue';
+import RouterTabs from './components/RouterTabs.vue';
+
+export default defineComponent({
+  components: {
+    RouterTabs
+  }
+});
+</script>
+
+<template lang='pug'>
+RouterTabs
+</template>
+
+<style scoped lang='sass'>
+
+</style>
